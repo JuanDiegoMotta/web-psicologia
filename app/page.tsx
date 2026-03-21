8f30092e4bf9308f9406a22dc4c8a0bb3124ad9d
Juan Diego Motta Moncada <mottajuandiego@gmail.com>
2026-03-21 18:57:42 +0100

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function HomePage() {
+  return (
+    <main className="p-8">
+      <h1 className="text-3xl font-bold text-blue-600">
+        ¡Hola, Clínica!
+      </h1>
+    </main>
+  );
+}
